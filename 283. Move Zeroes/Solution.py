2dda4b478c32ce68a8c18f1f29cbdd56954c2683
vishnu vardhan <68633446+vishnukoyyada@users.noreply.github.com>
2024-07-09 09:29:57 +0530

    Python3 Solution
            Submission Statistics:
            Question #: 283
            Language: Python3
            Runtime: 113 ms
            Runtime percentile: 97.43
            Memory: 18.124 MB
            Memory percentile: 16.01

--- 283. Move Zeroes/Solution.py
+++ 283. Move Zeroes/Solution.py
@@ -3,10 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        index =0
-        for i in range(len(nums)):
-            if(nums[i] !=0):
-                nums[index] =nums[i]
-                index+=1
-        for i in range(index,len(nums)):
-            nums[i]=0
+        left =0
+
+        for right in range(len(nums)):
+            if(nums[right] != 0):
+                nums[left],nums[right] = nums[right],nums[left]
+                left+=1
+
